6e97ffa52b094abf00ec6c03f2576310812b0377
hoangleminhtriet <141044158+hoangleminhtriet@users.noreply.github.com>
2024-02-27 12:21:54 +0700

    upadate css food screen

--- swp/src/screen/Food/FoodScreen.jsx
+++ swp/src/screen/Food/FoodScreen.jsx
@@ -5,7 +5,9 @@ export default function FoodScreen() {
   return (
     <div>
       <Container>
-        <h1 style={{ textAlign: "center" }}>Menu</h1>
+        <h1 style={{ textAlign: "center", color: "yellow" }}>
+          FPTBOOKING MENU
+        </h1>
         <Row className="test1">
           {/*Food1*/}
           <Col className="m-3" style={{ textAlign: "center" }}>
@@ -16,18 +18,25 @@ export default function FoodScreen() {
                 src="https://thebigbox.ca/wp-content/uploads/2020/04/traditional-cheese-pizza.png"
               />
               <Card.Body>
-                <Card.Title>Traditional Pizza</Card.Title>
+                <Card.Title className="title-food">
+                  Traditional Pizza
+                </Card.Title>
                 <Card.Text>Pepperoni / Hawaiian / Beef / Chicken</Card.Text>
-                <Row style={{ flexWrap: "nowrap", justifyContent: "center" }}>
-                  <Card className="food-price">
+                <Row
+                  style={{
+                    flexWrap: "nowrap",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Card className="food-price" style={{ marginRight: "5px" }}>
                     <Card.Title className="price-detail">10"</Card.Title>
-                    <Card.Text className="mb-0">$15.95</Card.Text>
-                    <Card.Text>+ GST</Card.Text>
+                    <Card.Text className="mb-0 price-title1">$15.95</Card.Text>
+                    <Card.Text className="price-title2">+ GST</Card.Text>
                   </Card>
                   <Card className="food-price">
                     <Card.Title className="price-detail">12"</Card.Title>
-                    <Card.Text className="mb-0">$15.95</Card.Text>
-                    <Card.Text>+ GST</Card.Text>
+                    <Card.Text className="mb-0 price-title1">$19.95</Card.Text>
+                    <Card.Text className="price-title2">+ GST</Card.Text>
                   </Card>
                 </Row>
               </Card.Body>
@@ -42,18 +51,20 @@ export default function FoodScreen() {
                 src="https://thebigbox.ca/wp-content/uploads/2020/04/traditional-cheese-pizza-1.png"
               />
               <Card.Body>
-                <Card.Title>Traditional Pizza</Card.Title>
+                <Card.Title className="title-food">
+                  Traditional Pizza
+                </Card.Title>
                 <Card.Text>Vegetarian / Cheese</Card.Text>
                 <Row style={{ flexWrap: "nowrap", justifyContent: "center" }}>
-                  <Card className="food-price">
+                  <Card className="food-price" style={{ marginRight: "5px" }}>
                     <Card.Title className="price-detail">10"</Card.Title>
-                    <Card.Text className="mb-0">$15.95</Card.Text>
-                    <Card.Text>+ GST</Card.Text>
+                    <Card.Text className="mb-0 price-title1">$15.95</Card.Text>
+                    <Card.Text className="price-title2">+ GST</Card.Text>
                   </Card>
                   <Card className="food-price">
                     <Card.Title className="price-detail">12"</Card.Title>
-                    <Card.Text className="mb-0">$15.95</Card.Text>
-                    <Card.Text>+ GST</Card.Text>
+                    <Card.Text className="mb-0 price-title1">$19.95</Card.Text>
+                    <Card.Text className="price-title2">+ GST</Card.Text>
                   </Card>
                 </Row>
               </Card.Body>
@@ -68,15 +79,15 @@ export default function FoodScreen() {
                 src="https://thebigbox.ca/wp-content/uploads/2020/04/wings.png"
               />
               <Card.Body>
-                <Card.Title>Chicken Wings</Card.Title>
+                <Card.Title className="title-food">Chicken Wings</Card.Title>
                 <Card.Text>
                   8 pieces served with veggies & ranch Hot / Teriyaki / Honey
                   Garlic / Salt & Pepper / Plain
                 </Card.Text>
                 <Row style={{ flexWrap: "nowrap", justifyContent: "center" }}>
                   <Card className="food-price">
-                    <Card.Text className="mb-0">$15.95</Card.Text>
-                    <Card.Text>+ GST</Card.Text>
+                    <Card.Text className="mb-0 price-title1">$15.95</Card.Text>
+                    <Card.Text className="price-title2">+ GST</Card.Text>
                   </Card>
                 </Row>
               </Card.Body>
@@ -94,25 +105,25 @@ export default function FoodScreen() {
                 src="https://thebigbox.ca/wp-content/uploads/2020/04/tender-fries.png"
               />
               <Card.Body>
-                <Card.Title>Chicken Tenders</Card.Title>
+                <Card.Title className="title-food">Chicken Tenders</Card.Title>
                 <Card.Text>
                   Sweet n’ Sour, Honey Mustard & BBQ Sauces available to choose
                   from.
                 </Card.Text>
                 <Row style={{ flexWrap: "nowrap", justifyContent: "center" }}>
-                  <Card className="food-price">
+                  <Card className="food-price" style={{ marginRight: "5px" }}>
                     <Card.Title className="price-detail">
                       Tenders only
                     </Card.Title>
-                    <Card.Text className="mb-0">$15.95</Card.Text>
-                    <Card.Text>+ GST</Card.Text>
+                    <Card.Text className="mb-0 price-title1">$6.95</Card.Text>
+                    <Card.Text className="price-title2">+ GST</Card.Text>
                   </Card>
                   <Card className="food-price">
                     <Card.Title className="price-detail">
                       w/ Fries & Drink
                     </Card.Title>
-                    <Card.Text className="mb-0">$15.95</Card.Text>
-                    <Card.Text>+ GST</Card.Text>
+                    <Card.Text className="mb-0 price-title1">$10.95</Card.Text>
+                    <Card.Text className="price-title2">+ GST</Card.Text>
                   </Card>
                 </Row>
               </Card.Body>
@@ -127,25 +138,25 @@ export default function FoodScreen() {
                 src="https://thebigbox.ca/wp-content/uploads/2020/04/hot-dog.png"
               />
               <Card.Body>
-                <Card.Title>Hot Dog</Card.Title>
+                <Card.Title className="title-food">Hot Dog</Card.Title>
                 <Card.Text>
                   Exotic Beef Hot Dog served in a slit of the partially sliced
                   bun.
                 </Card.Text>
                 <Row style={{ flexWrap: "nowrap", justifyContent: "center" }}>
-                  <Card className="food-price">
+                  <Card className="food-price" style={{ marginRight: "5px" }}>
                     <Card.Title className="price-detail">
                       Hot dog only
                     </Card.Title>
-                    <Card.Text className="mb-0">$15.95</Card.Text>
-                    <Card.Text>+ GST</Card.Text>
+                    <Card.Text className="mb-0 price-title1">$4.50</Card.Text>
+                    <Card.Text className="price-title2">+ GST</Card.Text>
                   </Card>
                   <Card className="food-price">
                     <Card.Title className="price-detail">
                       w/ Fries & Drink
                     </Card.Title>
-                    <Card.Text className="mb-0">$15.95</Card.Text>
-                    <Card.Text>+ GST</Card.Text>
+                    <Card.Text className="mb-0 price-title1  ">$9.90</Card.Text>
+                    <Card.Text className="price-title2">+ GST</Card.Text>
                   </Card>
                 </Row>
               </Card.Body>
@@ -160,7 +171,7 @@ export default function FoodScreen() {
                 src="https://thebigbox.ca/wp-content/uploads/2020/04/ultimate-nachos-01.png"
               />
               <Card.Body>
-                <Card.Title>Nachos Platter</Card.Title>
+                <Card.Title className="title-food">Nachos Platter</Card.Title>
                 <Card.Text>**THIS ITEM IS CURRENTLY UNAVAILABLE**</Card.Text>
                 <Card.Text>
                   Add a delicious meat topping. Options include Fajita Chicken
@@ -168,7 +179,9 @@ export default function FoodScreen() {
                 </Card.Text>
                 <Row style={{ flexWrap: "nowrap", justifyContent: "center" }}>
                   <Card className="food-price">
-                    <Card.Text>N/A</Card.Text>
+                    <Card.Text style={{ fontWeight: "bold", fontSize: "20px" }}>
+                      N/A
+                    </Card.Text>
                   </Card>
                 </Row>
                 <Card.Text>$1.99 + GST per topping</Card.Text>
@@ -187,12 +200,12 @@ export default function FoodScreen() {
                 src="https://thebigbox.ca/wp-content/uploads/2020/04/sandwich.png"
               />
               <Card.Body>
-                <Card.Title>Sandwiches</Card.Title>
+                <Card.Title className="title-food">Sandwiches</Card.Title>
 
                 <Row style={{ flexWrap: "nowrap", justifyContent: "center" }}>
                   <Card className="food-price">
-                    <Card.Text className="mb-0">$15.95</Card.Text>
-                    <Card.Text>+ GST</Card.Text>
+                    <Card.Text className="mb-0 price-title1">$7.95</Card.Text>
+                    <Card.Text className="price-title2">+ GST</Card.Text>
                   </Card>
                 </Row>
               </Card.Body>
@@ -207,12 +220,12 @@ export default function FoodScreen() {
                 src="https://thebigbox.ca/wp-content/uploads/2020/04/wrap.png"
               />
               <Card.Body>
-                <Card.Title>Wraps</Card.Title>
+                <Card.Title className="title-food">Wraps</Card.Title>
 
                 <Row style={{ flexWrap: "nowrap", justifyContent: "center" }}>
                   <Card className="food-price">
-                    <Card.Text className="mb-0">$15.95</Card.Text>
-                    <Card.Text>+ GST</Card.Text>
+                    <Card.Text className="mb-0 price-title1">$7.95</Card.Text>
+                    <Card.Text className="price-title2">+ GST</Card.Text>
                   </Card>
                 </Row>
               </Card.Body>
@@ -227,12 +240,14 @@ export default function FoodScreen() {
                 src="https://thebigbox.ca/wp-content/uploads/2020/04/fruit-plus-veggie.png"
               />
               <Card.Body>
-                <Card.Title>Veggie / Friut Cups</Card.Title>
+                <Card.Title className="title-food">
+                  Veggie / Friut Cups
+                </Card.Title>
 
                 <Row style={{ flexWrap: "nowrap", justifyContent: "center" }}>
                   <Card className="food-price">
-                    <Card.Text className="mb-0">$15.95</Card.Text>
-                    <Card.Text>+ GST</Card.Text>
+                    <Card.Text className="mb-0 price-title1">$5.95</Card.Text>
+                    <Card.Text className="price-title2">+ GST</Card.Text>
                   </Card>
                 </Row>
               </Card.Body>
@@ -247,12 +262,12 @@ export default function FoodScreen() {
                 src="https://thebigbox.ca/wp-content/uploads/2020/04/salad.png"
               />
               <Card.Body>
-                <Card.Title>Salads</Card.Title>
+                <Card.Title className="title-food">Salads</Card.Title>
 
                 <Row style={{ flexWrap: "nowrap", justifyContent: "center" }}>
                   <Card className="food-price">
-                    <Card.Text className="mb-0">$15.95</Card.Text>
-                    <Card.Text>+ GST</Card.Text>
+                    <Card.Text className="mb-0 price-title1">$6.95</Card.Text>
+                    <Card.Text className="price-title2">+ GST</Card.Text>
                   </Card>
                 </Row>
               </Card.Body>
